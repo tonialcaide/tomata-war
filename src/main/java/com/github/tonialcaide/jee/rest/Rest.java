81281458147187ec65f3c4180f5f04c60532ee17
Alcaide Navarro, Antonio <aalcaide@umivale.es>
2019-02-18 09:12:36 +0100

    Businnes logic layer invocation

--- src/main/java/com/github/tonialcaide/jee/rest/Rest.java
+++ src/main/java/com/github/tonialcaide/jee/rest/Rest.java
@@ -1,5 +1,8 @@
 package com.github.tonialcaide.jee.rest;
 
+import com.github.tonialcaide.tomata.ejb.TomataEJB;
+
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,11 +10,16 @@ import javax.ws.rs.Produces;
 @Path("/book")
 public class Rest {
 
+        @EJB
+        TomataEJB tomataEJB;
+
         @GET
         @Path("/test")
         @Produces("text/plain")
         public String getBookTitle() {
-            return "It works!";
+
+                return tomataEJB.test();
+
         }
 
 }
